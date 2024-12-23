4bc4a2e6d90e1cfa046115fe647be4c2282750ac
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-23 12:07:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+       let count=0;
+        let timer=document.getElementById("timer");
+        function displayClock()
+        {
+            let d=new Date()
+            let dd=d.getDate()
+            let mm=d.getMonth()
+            let yyyy = d.getFullYear();
+            let hh=d.getHours();
+            let min=d.getMinutes();
+            let ss=d.getSeconds()
+
+            timer.innerHTML = `${mm}/${dd}/${yyyy},${hh}:${min<10? ("0"+min):min}:${ss} ${hh>=12?"PM":"AM"}`
+        }
+        setInterval(displayClock,1000);
